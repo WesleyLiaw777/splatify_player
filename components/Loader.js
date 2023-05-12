4bc204b8533160a27795faba54f7fd857c053ab2
WesleyLiaw <wesleyliaw777@gmail.com>
2023-05-12 07:16:34 -0500

    removed loading logo

--- components/Loader.js
+++ components/Loader.js
@@ -5,14 +5,15 @@ function Loader() {
   return (
     <div className="h-screen bg-black">
       <div className="pt-40 flex flex-col items-center space-y-4">
-        <span className="relative w-[400px] h-[250px] lg:w-[550px] lg:h-[240px]">
+        {/* <span className="relative w-[400px] h-[250px] lg:w-[550px] lg:h-[240px]">
           <Image
-            src="https://rb.gy/y9mwtb"
+            src="https://1000logos.net/wp-content/uploads/2021/04/Spotify-logo.png"
             fill={true}
             style={{objectFit:"contain"}}
             className="animate-pulse"
+            alt="spotify logo"
           />
-        </span>
+        </span> */}
         <ThreeBounce size={23} color="#15883e" />
       </div>
     </div>
